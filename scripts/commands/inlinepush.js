5da3c33183c7492e5fee954a08fc0147ee0bfb8a
Anthony Catel <a.catel@weelya.com>
2011-06-17 10:24:38 -0700

    Quick fix : avoid memory leak in Ape.config() using inlinepush

--- scripts/commands/inlinepush.js
+++ scripts/commands/inlinepush.js
@@ -1,18 +1,21 @@
-Ape.registerCmd("inlinepush", false, function(params, infos) {
-	if (params.password == Ape.config("inlinepush.conf", "password")) {
+(function() {
+	var password = Ape.config("inlinepush.conf", "password");
+	Ape.registerCmd("inlinepush", false, function(params, infos) {
+		if (params.password == password) {
 		
-		if ($defined(params.channel) && $defined(params.data) && $defined(params.raw)) {
-			var chan = Ape.getChannelByName(params.channel);
-			if (!$defined(chan)) return ["401", "UNKNOWN_CHANNEL"];
+			if ($defined(params.channel) && $defined(params.data) && $defined(params.raw)) {
+				var chan = Ape.getChannelByName(params.channel);
+				if (!$defined(chan)) return ["401", "UNKNOWN_CHANNEL"];
 			
-			chan.pipe.sendRaw(params.raw, params.data);
+				chan.pipe.sendRaw(params.raw, params.data);
 			
-			return {"name":"pushed","data":{"value":"ok"}};
+				return {"name":"pushed","data":{"value":"ok"}};
+			} else {
+				return 0;
+			}
 		} else {
-			return 0;
+			return ["400", "BAD_PASSWORD"];
 		}
-	} else {
-		return ["400", "BAD_PASSWORD"];
-	}
 
-})
+	})
+})()
