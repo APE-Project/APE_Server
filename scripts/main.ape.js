41adc3ff7c5015674767bbb9166303a04285a17f
Peter Reijnders <Peter.Reijnders@verpeteren.nl>
2013-06-05 12:22:53 +0200

    main.ape.js more verbose error reporting

--- scripts/main.ape.js
+++ scripts/main.ape.js
@@ -1,18 +1,22 @@
 Ape.addEvent("init", function() {
-	include("framework/mootools.js");
-	include("framework/Http.js");
-	include("framework/userslist.js");
-	include("utils/utils.js");
-	include("commands/proxy.js");
-	include("commands/inlinepush.js");
-	include("examples/nickname.js");
-	include("examples/move.js");
-	include("utils/checkTool.js"); //Just needed for the APE JSF diagnostic tool, once APE is installed you can remove it 
-	//include("examples/ircserver.js");
-	//include("framework/http_auth.js");
-	
-	//Those file are used to test feature of APE.
-	//include("test/MySQL.js");
-	//include("test/Socket.js");
-	//include("test/Pipes.js");
+	try {
+			include("framework/mootools.js");
+			include("framework/Http.js");
+			include("framework/userslist.js");
+			include("utils/utils.js");
+			include("commands/proxy.js");
+			include("commands/inlinepush.js");
+			include("examples/nickname.js");
+			include("examples/move.js");
+			include("utils/checkTool.js"); //Just needed for the APE JSF diagnostic tool, once APE is installed you can remove it 
+			//include("examples/ircserver.js");
+			//include("framework/http_auth.js");
+			
+			//Those file are used to test feature of APE.
+			//include("test/MySQL.js");
+			//include("test/Socket.js");
+			//include("test/Pipes.js");
+	} catch (e) {
+		Ape.log(e.message + '\n\t' + e.fileName + ':' + e.lineNumber);
+	}
 });
