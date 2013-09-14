77fa9e3c9b00e4caf5ff5de5ca4dd83404b05150
Peter Reijnders <Peter.Reijnders@verpeteren.nl>
2013-09-14 13:53:21 +0200

    added test for hostname resolving

--- scripts/test/Hostname.js
+++ scripts/test/Hostname.js
@@ -1,7 +1,20 @@
 Ape.log(' =====================================>>> \n Start up for test/Hostname.js\n');
 try {
+//	Ape.log(' =====================================>>> \n Starting test Ape.getHostByName\n');
+//	var r = Ape.getHostByName('www.verpeteren.nl');
+//	Ape.log('returned: ' + r);
+	Ape.log(' =====================================>>> \n Starting test Ape.os.getHostByName\n');
 	var r = os.getHostByName('www.verpeteren.nl');
 	Ape.log('returned: ' + r);
+	Ape.log(' =====================================>>> \n Starting test Ape.os.resolveHostByName\n');
+	var r = os.resolveHostByName('www.verpeteren.nl', function(ip) {
+		Ape.log('resolved: ' + ip);
+	});
+	Ape.log(' =====================================>>> \n Starting test fake Ape.os.resolveHostByName\n');
+	var r = os.resolveHostByName('www.this.should.not.work.@all', function(ip) {
+		Ape.log('resolved: ' + ip);
+	});
+
 } catch (e) {
 	Ape.log(e.message + '\n\t' + e.fileName + ':' + e.lineNumber);
 }
